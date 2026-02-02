d32b4e6fdf4bc88b9c42eaf89f63b4c127629936
Manhitha Thineti <manhithathineti@mac.mynetworksettings.com>
2026-02-02 16:04:30 -0500

    pushing all the bank project code

---
+++ users/urls.py
@@ -0,0 +1,5 @@
+from .views import account_opening
+from django.urls import path
+urlpatterns = [
+    path('', account_opening, name='home')
+]
---
+++ users/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def account_opening(request):
+    return render(request, 'users/account_opening.html')
